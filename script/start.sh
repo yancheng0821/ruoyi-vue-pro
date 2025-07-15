e194d0c009a8b8d98d8449aa2089a9678f3644c3
yancheng0821 <33597193+yancheng0821@users.noreply.github.com>
2025-07-14 21:53:02 -0700

    Update start.sh

--- script/start.sh
+++ script/start.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 echo "Starting application..."
 
-JAR_PATH="yudao-server/target/yudao-server-*.jar"
+JAR_PATH="/home/ec2-user/app/yudao-server.jar"
 
-# 终止旧进程（可选）
+# 终止旧进程
 PID=$(pgrep -f "$JAR_PATH")
 if [ -n "$PID" ]; then
   echo "Killing existing process $PID"
@@ -11,5 +11,5 @@ if [ -n "$PID" ]; then
 fi
 
 # 启动新进程
-nohup java -jar $JAR_PATH > /home/ec2-user/app/app.log 2>&1 &
+nohup java -jar "$JAR_PATH" > /home/ec2-user/app/app.log 2>&1 &
 echo "Application started"
